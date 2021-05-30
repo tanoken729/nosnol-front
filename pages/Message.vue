d41cdb2b9fbcd9d6506700d33b46e755c3cd99ef
たのけん <tanoken729@gmail.com>
2021-05-30 13:42:04 +0900

    メッセージ画面を作成

---
+++ pages/Message.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="wrapper">
+  <headerAfterLogin />
+    <main class="main">
+        <h2>メッセージ</h2>
+        <p>ユーザー名</p>
+        <p>ユーザー名</p>
+        <h2>ユーザー名</h2>
+        <input type="text" placeholder="新しいメッセージを作成">
+        <button>送信</button>
+    </main>
+</div>
+</template>
+
+<script>
+import headerAfterLogin from "@/components/headerAfterLogin.vue";
+
+export default {
+  components: {
+    headerAfterLogin,
+  }
+};
+</script>
+
+<style scoped>
+/* * {
+  outline: solid 1px #000;
+} */
+.main {
+  display: flex;
+}
+</style>

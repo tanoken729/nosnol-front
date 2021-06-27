bddb56abb608738a26009e30ea5a0dc61dcf11e3
たのけん <tanoken729@gmail.com>
2021-06-27 20:52:56 +0900

    ログイン後問題なくリダイレクトされるようにする

--- pages/Signin.vue
+++ pages/Signin.vue
@@ -70,7 +70,7 @@ export default {
           this.$router.push('/TopAfterLogin')
         } catch(error){
           console.log(error)
-          redirect('/')
+          this.$router.push('/signin')
         }
     }
   },

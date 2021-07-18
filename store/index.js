de85fa039cca9f2da2d4593a54e2e1330b1444eb
たのけん <tanoken729@gmail.com>
2021-07-18 13:05:31 +0900

    store雛形作成

--- store/index.js
+++ store/index.js
@@ -5,40 +5,23 @@ const store = () => {
             musicFileData: []
         }),
         getters: {
-            list(state) {
+            musicFileData(state) {
                 return state.musicFileData
             }
         },
         mutations: {
-            addMusicFileData(state, data) {
-                state.musicFileData.push({
-                    musicFile: data.musicFile,
-                    coverImage: data.coverImage,
-                    title: data.title,
-                    genre: data.genre,
-                    emotions: data.emotions,
-                    userId: data.userId,
-                })
-            },
-            getMusicFileData(state, data) {
-                state.musicFileData = data
-            }
+            setMusicFileData(state, response) {
+                state.musicFileData = response
+Ω            },
         },
         actions: {
-            async addMusicFileDataAction({ commit }, data) {
-                const response = await this.$axios.$post('/musicFileData', data)
-                    .catch(err => {
-                        console.log(err);
-                    })
-                commit('addMusicFileData', response)
-            },
-            async getMusicFileDataAction() {
-                return await this.$axios.$get('/musicFileData')
-                    .catch(err => {
-                        console.log(err);
-                    })
+            async setMusicFileData(commit) {
+                const response = await this.$axios.$get('api/musicFileData')
+                commit('setMusicFileData', response)
+                console.log('成功')
             },
         }
     })
 }
+
 export default store

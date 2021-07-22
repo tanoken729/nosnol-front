fb5293807b7bc634e4e78f48ed302fba65be33f0
たのけん <tanoken729@gmail.com>
2021-07-23 01:25:14 +0900

    update top page style

--- pages/index.vue
+++ pages/index.vue
@@ -4,102 +4,88 @@
     <main class="main">
       <nav class="side-bar">
         <h3>感情</h3>
-        <ul class="emotion">
-          <li class="joy">喜</li>
-          <li class="angry">怒</li>
-          <li class="sorrow">哀</li>
-          <li class="easy">楽</li>
-        </ul>
+          <div class="emotion">
+            <input type="radio" name="emotion" value="joy" id="joy"><label for="joy" class="label">喜</label>
+            <input type="radio" name="emotion" value="angry" id="angry"><label for="angry" class="label">怒</label>
+            <input type="radio" name="emotion" value="sorrow" id="sorrow"><label for="sorrow" class="label">哀</label>
+            <input type="radio" name="emotion" value="easy" id="easy"><label for="easy" class="label">楽</label>
+          </div>
         <h3>ジャンル</h3>
-        <ul class="genre">
-          <li>J-POP</li>
-          <li>アニメ</li>
-          <li>邦楽ヒップホップ/R&B/レゲエ</li>
-          <li>邦楽ロック</li>
-          <li>邦楽ダンス/エレクトロニカ</li>
-          <li>K-POP/ワールド・ミュージック</li>
-          <li>洋楽総合</li>
-          <li>洋楽ポップス</li>
-          <li>洋楽ヒップホップ/R&B/レゲエ</li>
-          <li>洋楽ロック</li>
-          <li>洋楽ダンス/エレクトロニカ</li>
-          <li>歌謡曲/演歌</li>
-          <li>ジャズ</li>
-          <li>クラシック</li>
-        </ul>
+          <div class="genre">
+            <input type="radio" name="genre" value="j-pop" id="j-pop"><label for="j-pop" class="genre-label">J-POP</label>
+            <input type="radio" name="genre" value="anime" id="anime"><label for="anime" class="genre-label">アニメ</label>
+            <input type="radio" name="genre" value="hiphop-rnb-reggae" id="hiphop-rnb-reggae"><label for="hiphop-rnb-reggae" class="genre-label">ヒップホップ/R&B/レゲエ</label>
+            <input type="radio" name="genre" value="rock" id="rock"><label for="rock" class="genre-label">ロック</label>
+            <input type="radio" name="genre" value="dance-electronica" id="dance-electronica"><label for="dance-electronica" class="genre-label">ダンス/エレクトロニカ</label>
+            <input type="radio" name="genre" value="k-pop" id="k-pop"><label for="k-pop" class="genre-label">K-POP/ワールド・ミュージック</label>
+            <input type="radio" name="genre" value="western-music" id="western-music"><label for="western-music" class="genre-label">洋楽総合</label>
+            <input type="radio" name="genre" value="western-pop" id="western-pop"><label for="western-pop" class="genre-label">洋楽ポップス</label>
+            <input type="radio" name="genre" value="kayokyoku-enka" id="kayokyoku-enka"><label for="kayokyoku-enka" class="genre-label">歌謡曲/演歌</label>
+            <input type="radio" name="genre" value="jazz" id="jazz"><label for="jazz" class="genre-label">ジャズ</label>
+            <input type="radio" name="genre" value="classic" id="classic"><label for="classic" class="genre-label">クラシック</label>
+          </div>
       </nav>
-      <div class="content">
-        <NuxtLink to="/musicfiledetail">
-          <p><audio src="" controls><source src="op.mp3" type="audio/mp3"></audio></p>
-        </NuxtLink>
-        <NuxtLink to="/musicfiledetail">
-          <p><audio src="" controls></audio></p>
-        </NuxtLink>
-        <NuxtLink to="/musicfiledetail">
-          <p><audio src="" controls></audio></p>
-        </NuxtLink>
-        <NuxtLink to="/musicfiledetail">
-          <p><audio src="" controls></audio></p>
-        </NuxtLink>
-        <NuxtLink to="/musicfiledetail">
-          <p><audio src="" controls></audio></p>
-        </NuxtLink>
-        <div id="app">
-          <aplayer :music="music" :list="list"></aplayer>
+      <div class="content-fit">
+      <NuxtLink to="/musicfiledetail">
+      <div class="content" v-for="(item, index) in items" :key="index" @click="setMusicFileData">
+        <div>
+            <img :src="`${$axios.defaults.baseURL}storage/${item.cover_image}`" class="cover-image">
+            <h3>{{ item.title }}</h3>
+            <audio controls>
+              <source :src="`${$axios.defaults.baseURL}storage/${item.music_file}`" type="audio/mp3">
+            </audio>
         </div>
-        <h2>Result</h2>
-        {{ this.message }}
+      </div>
+      </NuxtLink>
       </div>
     </main>
 </div>
 </template>
 
 <script>
-import Header from "@/components/header.vue";
-import axios from 'axios'
-//document is not definedになるためコメントアウト
-// import Aplayer from 'vue-aplayer';
-// require('vue-aplayer')
+import headerAfterLogin from "@/components/headerAfterLogin.vue";
+import store from '../store';
 
 export default {
+  // middleware: 'user_auth',
   components: {
-    Header,
-    //document is not definedになるためコメントアウト
-    // Aplayer
+    headerAfterLogin,
   },
-  data() {
+  data(){
     return {
-      message: '',
-      music: {
-        title: 'secret base~君がくれたもの~',
-        artist: 'Silent Siren',
-        src: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3',
-        pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
-      },
-      list: [
-        {
-          title: 'secret base~君がくれたもの~',
-          artist: 'Silent Siren',
-          src: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3',
-          pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
-        },
-      {title:"前前前世",artist:"RADWIMPS",src:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yourname.mp3",pic:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yourname.jpg",lrc:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yourname.lrc"},
-      {title:"光るなら.m3u8",artist:"Goose house",src:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/hls/hikarunara.m3u8",pic:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/hikarunara.jpg",lrc:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/hikarunara.lrc"},
-      {title:"回レ！雪月花",artist:"小倉唯",src:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/snowmoonflowers.mp3",pic:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/snowmoonflowers.jpg",lrc:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/snowmoonflowers.lrc"},
-      {title:"あっちゅ～ま青春!",artist:"七森中☆ごらく部",src:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yuruyuri.mp3",pic:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yuruyuri.jpg",lrc:"https://cn-east-17-aplayer-35525609.oss.dogecdn.com/yuruyuri.lrc"}
-      ]
+      musicFile: "",
+      coverImage: "",
+      title: "",
+      genre: "",
+      emotions: "",
+      userId: "",
+      items: []
     }
   },
-  mounted () {
-  axios.get('http://localhost:8000/api/test')
-    .then((res) => {
-      console.log(res.data)
-      this.message = res.data
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
+  mounted: function() {
+    this.$axios
+      .$get('api/musicFileData')
+      .then(response => {
+        this.items = response
+        // console.log(this.items)
+      })
+      .catch(error => {
+        // console.log(error)
+      })
+  },
+  methods: {
+    setMusicFileData () {
+      this.$store.dispatch('setMusicFileData', {
+        items: this.items
+      })
+      console.log(this.items)
+    }
+  },
+  // computed: {
+  //   items: function () {
+  //     return this.$store.getters.musicFileData
+  //   },
+  // },
 };
 </script>
 
@@ -134,12 +120,12 @@ export default {
 }
 /* 感情 */
 .emotion li {
-  border: 1px solid #d4d3d3;
+  /* border: 1px solid #d4d3d3;
   margin-top: 10px;
   margin-bottom: 10px;
   border-radius: 0.5rem;
   width: 50px;
-  text-align: center;
+  text-align: center; */
 }
 .joy:hover {
   border: 1px solid #FF7F00;
@@ -192,9 +178,11 @@ export default {
 .content {
   /* display: flex; */
   padding: 10px;
+  /* height: 200px;
+  width: 295px; */
 }
-.content p {
-  padding-top: 70px;
+.content {
+  /* padding-top: 70px; */
   border-radius: 0.5rem;
   border: 1px solid #d4d3d3;
   display: inline-block;
@@ -203,9 +191,11 @@ export default {
 	transition: .3s;
   /* background-color: #e5e9f7; */
   color: #696969;
+  padding: 5px;
+  /* background-color: rgb(230, 231, 252); */
 }
-.content p:hover {
-  padding-top: 70px;
+.content:hover {
+  /* padding-top: 70px; */
   border-radius: 0.5rem;
   border: 1px solid #d4d3d3;
   display: inline-block;
@@ -215,8 +205,79 @@ export default {
   /* background-color: #e5e9f7; */
   color: #696969;
 }
+.cover-image {
+  height: 200px;
+  width: 200px;
+  display: flex;
+}
 audio {
-  width: 250px;
+  width: 200px;
   /* height: 50px; */
+  margin-top: 5px;
+  /* background-color: #000CFF; */
+}
+.content-fit {
+  max-width: 100%;
+  margin: 10px;
+}
+
+.emotion {
+  list-style: none;
+  margin: 0 auto;
+}
+input[type=radio] {
+  display: none;
+}
+input[type="radio"]:checked + .label {
+  border: 1px solid #d4d3d3;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 0.5rem;
+  width: 50px;
+  color: #000CFF;
+  background-color: #e5e9f7;
+  font-weight: bold;
+  padding: 10px 10px;
+  margin: 10px auto;
+}
+.label {
+  border: 1px solid #d4d3d3;
+  margin-bottom: 10px;
+  border-radius: 0.5rem;
+  width: 50px;
+  text-align: center;
+  padding: 10px 10px;
+  margin: 10px auto;
+  display:block;
+  position:relative;
+}
+.label:hover {
+  color: #000CFF;
+  background-color: #e5e9f7;
+  margin: 10px auto;
+}
+
+input[type="radio"]:checked + .genre-label {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 0.5rem;
+  color: #000CFF;
+  background-color: #e5e9f7;
+  font-weight: bold;
+  padding: 10px 10px;
+  margin: 10px auto;
+}
+.genre-label {
+  margin-bottom: 10px;
+  border-radius: 0.5rem;
+  padding: 10px 10px;
+  margin: 10px auto;
+  display:block;
+  position:relative;
+}
+.genre-label:hover {
+  color: #000CFF;
+  background-color: #e5e9f7;
+  margin: 10px auto;
 }
 </style>

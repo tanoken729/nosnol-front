829a704ea692abde73921237dd42907e1ffd4fc7
たのけん <tanoken729@gmail.com>
2021-08-28 17:45:12 +0900

    get the data of the music file detail page at once

--- store/musicFiles.js
+++ store/musicFiles.js
@@ -1,13 +1,19 @@
 export const state = () => ({
   musicFileData: [],
-  clickedFileUserId: '',
-  clickedLoginUserId: '',
+  // clickedFileUserId: '',
+  // clickedLoginUserId: '',
+  followedId:'',
+  userId:'',
+  commentInfos:''
 })
 
 export const getters = {
   musicFileData: state => state.musicFileData,
-  clickedFileUserId: state => state.clickedFileUserId,
-  clickedLoginUserId: state => state.clickedLoginUserId,
+  // clickedFileUserId: state => state.clickedFileUserId,
+  // clickedLoginUserId: state => state.clickedLoginUserId,
+  followedId: (state) => state.followedId,
+  userId: (state) => state.userId,
+  commentInfos: (state) => state.commentInfos
 }
 
 export const mutations = {
@@ -16,14 +22,19 @@ export const mutations = {
     // console.log(state.musicFileData)
     state.musicFileData = musicFileData
   },
-  setFileUserId(state, clickedFileUserId) {
-    // console.log(state.clickedFileUserId)
-    state.clickedFileUserId = clickedFileUserId
-  },
-  setLoginUserId(state, clickedLoginUserId) {
-    console.log(state.clickedLoginUserId)
-    state.clickedLoginUserId = clickedLoginUserId
-  },
+  setMusicDetailPageData (state, { followedId, userId, commentInfos }) {
+    state.followedId = followedId
+    state.userId = userId
+    state.commentInfos = commentInfos
+  }
+  // setFileUserId(state, clickedFileUserId) {
+  //   // console.log(state.clickedFileUserId)
+  //   state.clickedFileUserId = clickedFileUserId
+  // },
+  // setLoginUserId(state, clickedLoginUserId) {
+  //   console.log(state.clickedLoginUserId)
+  //   state.clickedLoginUserId = clickedLoginUserId
+  // },
 }
 
 export const actions = {
@@ -38,11 +49,48 @@ export const actions = {
     }
     const musicFileData = [];
     musicFileData.push(musicFiledatum)
-    // console.log(musicFiledatum)
     context.commit('setMusicFileDataMutations', musicFileData)
   },
-  follow(context, payload) {
-    context.commit('setFileUserId', payload.clickedFileUserId)
-    context.commit('setLoginUserId', payload.clickedLoginUserId)
-  }
+  // follow(context, payload) {
+  //   context.commit('setFileUserId', payload.clickedFileUserId)
+  //   context.commit('setLoginUserId', payload.clickedLoginUserId)
+  // },
+  async musicDetailPageData(context, payload) {
+    await this.$axios.$get(`api/${payload.clickedLoginUserId}/${payload.clickedFileId}/${payload.clickedFileUserId}/musicDetailPageData`)
+    .then(response => {
+      const musicDetailPageData = response
+      // フォロー
+      let followedId = ''
+      const id = payload.clickedFileUserId
+      const followedIdCheck = musicDetailPageData.musicDetailPageData.some((v) => v.followed_id === id);
+        if (followedIdCheck) {
+          followedId = musicDetailPageData.musicDetailPageData[0].followed_id
+          console.log('follow ifだよ')
+        } else {
+          followedId = false
+          console.log('follow elseだよ')
+        }
+      // いいね
+      let userId = ''
+      const likeId = payload.clickedLoginUserId
+      const userIdCheck = musicDetailPageData.musicDetailPageData.some((v) => v.user_id === likeId);
+        if (userIdCheck) {
+          userId = musicDetailPageData.musicDetailPageData[0].user_id
+          console.log('like ifだよ')
+        } else {
+          userId = false
+          console.log('like elseだよ')
+        }
+        // コメント
+        let commentInfos = musicDetailPageData.musicDetailPageData
+        // const textCheck = musicDetailPageData.musicDetailPageData.some((v) => v.tex);
+        // if (textCheck) {
+        //   commentInfos = musicDetailPageData.musicDetailPageData
+        // } else {
+        //   commentInfos = false
+        // }
+        // console.log(musicDetailPageData.musicDetailPageData.some((v) => v.tex))
+        context.commit('setMusicDetailPageData', { followedId: followedId, userId: userId, commentInfos: commentInfos })
+    })
+  },
 }

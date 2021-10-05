47e23761496e46ba402c979c25891b834a6123ba
たのけん <tanoken729@gmail.com>
2021-10-06 01:51:41 +0900

    あとでいらない部分消す、とりあえずログイン後の画面とスタイル統一する

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,23 @@
     <main class="main">
       <sideBar />
       <div class="content-fit">
-      <NuxtLink to="/musicfiledetail">
-      <div class="content" v-for="(item, index) in items" :key="index" @click="setMusicFileData">
+      <div class="content" v-for="(item, index) in $store.getters['musicFiles/items'].items" :key="index" @click="setMusicFileData(item.title, item.cover_image, item.music_file, item.user_name, item.user_id, item.id)">
+      <nuxt-link :to="{ name: 'user-title', params: {user: `${item.user_name}`, title: `${item.title}`} }">
         <div>
             <img :src="`${$axios.defaults.baseURL}storage/${item.cover_image}`" class="cover-image">
-            <h3>{{ item.title }}</h3>
-            <audio controls>
-              <source :src="`${$axios.defaults.baseURL}storage/${item.music_file}`" type="audio/mp3">
+            <h3 class="item-title">{{ item.title }}</h3>
+            <nuxt-link :to="{ name: 'user', params: {user: `${item.user_name}`} }"><h3 class="item-user-name">{{ item.user_name }}</h3></nuxt-link>
+            <audio v-bind:id="`bgm-${index}`" preload>
+              <source
+                :src="`${$axios.defaults.baseURL}storage/${item.music_file}`"
+                type="audio/mp3"
+              >
             </audio>
         </div>
+      </nuxt-link>
+            <button v-if="play === index" @click="pauseAction(index)" id="btn-play" type="button"><font-awesome-icon :icon="['fas', 'pause']"/></button>
+            <button v-else @click="playAction(index)" id="btn-play" type="button"><font-awesome-icon :icon="['fas', 'play']"/></button>
       </div>
-      </NuxtLink>
       </div>
     </main>
 </div>
@@ -37,32 +43,53 @@ export default {
       genre: "",
       emotions: "",
       userId: "",
-      items: []
+      items: [],
+      clickedFileTitle: '',
+      clickedFileCoverImage: '',
+      clickedFileId: '',
+      play: false,
     }
   },
-  mounted: function() {
-    this.$axios
-      .$get('api/musicFileData')
-      .then(response => {
-        this.items = response
-        // console.log(this.items)
-      })
-      .catch(error => {
-        // console.log(error)
-      })
+  asyncData: async function(context) {
+      // すべての音声ファイルを取得する
+    context.store.dispatch('musicFiles/musicFileTopPageData')
   },
   methods: {
-    setMusicFileData () {
-      this.$store.dispatch('setMusicFileData', {
-        items: this.items
+    setMusicFileData (clickedFileTitle, clickedFileCoverImage, clickedFileMusicfile, clickedFileUserName, clickedFileUserId, clickedFileId) {
+      this.clickedFileTitle = clickedFileTitle
+      this.clickedFileCoverImage = clickedFileCoverImage
+      this.clickedFileMusicfile = clickedFileMusicfile
+      this.clickedFileUserName = clickedFileUserName
+      this.clickedFileUserId = clickedFileUserId
+      this.clickedFileId = clickedFileId
+      this.$store.dispatch('musicFiles/setMusicFileData', {
+        clickedFileTitle: this.clickedFileTitle,
+        clickedFileCoverImage: this.clickedFileCoverImage,
+        clickedFileMusicfile: this.clickedFileMusicfile,
+        clickedFileUserName: this.clickedFileUserName,
+        // フォローで渡すためのやつ
+        clickedFileUserId: this.clickedFileUserId,
+        clickedFileId: this.clickedFileId,
       })
-      console.log(this.items)
-    }
+    },
+    playAction (index) {
+      this.play = index
+      this.bgm = index
+      console.log(this.bgm)
+      var audios = document.getElementById(`bgm-${index}`);
+      console.log(audios)
+      audios.play();
+    },
+    pauseAction (index) {
+      this.play = false
+      this.bgm = index
+      console.log(this.bgm)
+      var audios = document.getElementById(`bgm-${index}`);
+      console.log(audios)
+      audios.pause();
+    },
   },
   // computed: {
-  //   items: function () {
-  //     return this.$store.getters.musicFileData
-  //   },
   // },
 };
 </script>
@@ -72,20 +99,24 @@ export default {
   outline: solid 1px #000;
 } */
 /* メイン */
+/* * {
+  background: #333333;
+} */
+#btn-play {
+  padding: 15px 17px;
+  border-radius: 5rem;
+}
 .main {
   display: flex;
+  width: 1200px;
+  max-width: 100%;
+  margin: 0 auto;
 }
 /* メインコンテンツ */
 .content {
-  /* display: flex; */
-  padding: 10px;
-  /* height: 200px;
-  width: 295px; */
-}
-.content {
   /* padding-top: 70px; */
   border-radius: 0.5rem;
-  border: 1px solid #d4d3d3;
+  /* border: 1px solid #d4d3d3; */
   display: inline-block;
   margin: 10px;
   /* box-shadow: 0 0 3px 0 rgba(0,0,0,.12), 0 2px 3px 0 rgba(0,0,0,.22); */
@@ -94,22 +125,45 @@ export default {
   color: #696969;
   padding: 5px;
   /* background-color: rgb(230, 231, 252); */
+  position: relative;
 }
+.content button {
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  -ms-transform: translate(-50%,-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  /*以下装飾*/
+  margin:0;/*余計な隙間を除く*/
+  font-size: 20px;/*文字サイズ*/
+  border: none; /*線で囲う*/
+  padding: 7px;/*文字と線の間の余白*/
+  color: white;/*文字色*/
+  text-decoration: none;/*下線を表示させない*/
+  background: rgba(255, 255, 255, 0.3);/*背景を半透明に*/
+}
+.content button:hover{/*カーソルを当てたとき*/
+  background: linear-gradient(to right, rgb(38, 0, 255), rgb(0, 140, 255));
+  }
+
 .content:hover {
   /* padding-top: 70px; */
   border-radius: 0.5rem;
-  border: 1px solid #d4d3d3;
+  /* border: 1px solid #d4d3d3; */
   display: inline-block;
   margin: 10px;
-  box-shadow: 0 0 10px 0 rgba(0,0,0,.22);
+  /* box-shadow: 0 0 10px 0 rgba(0,0,0,.22); */
 	transition: .3s;
   /* background-color: #e5e9f7; */
   color: #696969;
 }
 .cover-image {
-  height: 200px;
-  width: 200px;
+  height: 150px;
+  width: 150px;
   display: flex;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 10px 0 rgba(0,0,0,.22);
 }
 audio {
   width: 200px;
@@ -120,5 +174,24 @@ audio {
 .content-fit {
   max-width: 100%;
   margin: 10px;
+  width: 1030px;
+}
+.item-title {
+  font-size: 16px;
+  color: #333333;
+  height: 25px;
+  width: 150px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  /* font-weight: bold; */
+  /* font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; */
+}
+.item-user-name {
+  font-size: 14px;
+  color: #999999;
+}
+a {
+  text-decoration: none;
 }
 </style>
